de0de615dbbe1243a9c6b9d2af69a5080bad66d1
Khatera Mobariz <khateramobariz@Khateras-MacBook-Pro.local>
2022-01-04 21:41:58 -0500

    test test

--- src/BranchExample.java
+++ src/BranchExample.java
@@ -3,7 +3,13 @@ public class BranchExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	}
+	
+		public void testA() {
+			System.out.println(" ");
+			
+		}
 
 	}
 
-}
+
